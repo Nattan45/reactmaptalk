7d3c11877648f2b4371b545f857c60ab5ecc40d1
HP.45 <you@example.com>
2024-09-04 15:50:51 +0300

    create: route on map

--- src/page/MaptalksRoutePage.jsx
+++ src/page/MaptalksRoutePage.jsx
@@ -1,42 +1,36 @@
-import React from 'react'
-import Sidebar from '../components/sidebar/Sidebar'
-import Footer from '../components/footer/Footer'
-import MaptalksRoute from '../components/maptalks/MaptalksRoute'
+import React from "react";
+import Sidebar from "../components/sidebar/Sidebar";
+import Footer from "../components/footer/Footer";
+import MaptalksRoute from "../components/maptalks/MaptalksRoute";
 
 const MaptalksRoutePage = () => {
   return (
     <div className="App">
-    {/* header */}
-    <div className="App-header">
-      <h1>Header Section</h1>
-    </div>
-
-
-    {/* Page-Container */}
-    <div className='pageContainer'>
-      {/* Side Bar */}
-      <div className='sideBar'>
-        <Sidebar/>
-        <hr/>
+      {/* header */}
+      <div className="App-header">
+        <h1>Header Section</h1>
       </div>
 
-      
+      {/* Page-Container */}
+      <div className="pageContainer">
+        {/* Side Bar */}
+        <div className="sideBar">
+          <Sidebar />
+          <hr />
+        </div>
 
-      {/* All Contents */}
-      <div className='contentsContainer'>
-        <MaptalksRoute/>
+        {/* All Contents */}
+        <div className="contentsContainer">
+          <MaptalksRoute />
+        </div>
       </div>
 
+      {/* Footer-Container */}
+      <div className="footerContainer">
+        <Footer />
+      </div>
     </div>
+  );
+};
 
-
-    {/* Footer-Container */}
-    <div className='footerContainer'>
-      <Footer/>
-    </div>
-  </div>
-
-  )
-}
-
-export default MaptalksRoutePage
+export default MaptalksRoutePage;
