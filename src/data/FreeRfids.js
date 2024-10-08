539ce357df770b8d55d0a72d8864bf52f536ccb2
nati <112703867+Nattan45@users.noreply.github.com>
2024-10-08 15:06:16 +0300

    update: rfid data type, creation form and data rendering

--- src/data/FreeRfids.js
+++ src/data/FreeRfids.js
@@ -1,198 +1,276 @@
 const FreeRfids = [
   {
     id: 1,
-    RfidKey: "12345",
-    status: "occupied",
+    RfidKey: "12345", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 2,
-    RfidKey: "56751",
-    status: "occupied",
+    RfidKey: "56751", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 3,
-    RfidKey: "5443",
-    status: "occupied",
+    RfidKey: "5443", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 4,
-    RfidKey: "56753",
-    status: "occupied",
+    RfidKey: "56753", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 5,
-    RfidKey: "5688",
-    status: "free",
+    RfidKey: "5688", // Auto Generated
+    tagType: "Passive", // Active / Pas
+    installationDate: "",
+    status: "free", // Free / Occupied
   },
   {
     id: 6,
-    RfidKey: "6262",
-    status: "free",
+    RfidKey: "6262", // Auto Generated
+    tagType: "Passive", // Active / Pas
+    installationDate: "",
+    status: "free", // Free / Occupied
   },
   {
     id: 7,
-    RfidKey: "7227",
-    status: "occupied",
+    RfidKey: "7227", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 8,
-    RfidKey: "8838",
-    status: "free",
+    RfidKey: "8838", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 9,
-    RfidKey: "9993",
-    status: "occupied",
+    RfidKey: "9993", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 10,
-    RfidKey: "2352",
-    status: "occupied",
+    RfidKey: "2352", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 11,
-    RfidKey: "2363",
-    status: "occupied",
+    RfidKey: "2363", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 12,
-    RfidKey: "2223",
-    status: "occupied",
+    RfidKey: "2223", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 13,
-    RfidKey: "2323",
-    status: "occupied",
+    RfidKey: "2323", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 14,
-    RfidKey: "3333",
-    status: "free",
+    RfidKey: "3333", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 15,
-    RfidKey: "4444",
-    status: "free",
+    RfidKey: "4444", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 16,
-    RfidKey: "5555",
-    status: "occupied",
+    RfidKey: "5555", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 17,
-    RfidKey: "6666",
-    status: "occupied",
+    RfidKey: "6666", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 18,
-    RfidKey: "77777",
-    status: "free",
+    RfidKey: "77777", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 19,
-    RfidKey: "88888",
-    status: "free",
+    RfidKey: "88888", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 20,
-    RfidKey: "99999",
-    status: "occupied",
+    RfidKey: "99999", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 21,
-    RfidKey: "10101",
-    status: "free",
+    RfidKey: "10101", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 22,
-    RfidKey: "20202",
-    status: "occupied",
+    RfidKey: "20202", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 23,
-    RfidKey: "30303",
-    status: "occupied",
+    RfidKey: "30303", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 24,
-    RfidKey: "40404",
-    status: "occupied",
+    RfidKey: "40404", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 25,
-    RfidKey: "50505",
-    status: "free",
+    RfidKey: "50505", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 26,
-    RfidKey: "60606",
-    status: "occupied",
+    RfidKey: "60606", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 27,
-    RfidKey: "70707",
-    status: "occupied",
+    RfidKey: "70707", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 28,
-    RfidKey: "80808",
-    status: "occupied",
+    RfidKey: "80808", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 29,
-    RfidKey: "90909",
-    status: "occupied",
+    RfidKey: "90909", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 30,
-    RfidKey: "10111",
-    status: "occupied",
+    RfidKey: "10111", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 31,
-    RfidKey: "10911",
-    status: "occupied",
+    RfidKey: "10911", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 32,
-    RfidKey: "10191",
-    status: "occupied",
+    RfidKey: "10191", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 33,
-    RfidKey: "19111",
-    status: "occupied",
+    RfidKey: "19111", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 34,
-    RfidKey: "90111",
-    status: "occupied",
+    RfidKey: "90111", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 35,
-    RfidKey: "10181",
-    status: "occupied",
+    RfidKey: "10181", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 36,
-    RfidKey: "18111",
-    status: "occupied",
+    RfidKey: "18111", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 37,
-    RfidKey: "10811",
-    status: "occupied",
+    RfidKey: "10811", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 38,
-    RfidKey: "10118",
-    status: "occupied",
+    RfidKey: "10118", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
   {
     id: 39,
-    RfidKey: "10199",
-    status: "free",
+    RfidKey: "10199", // Auto Generated
+    tagType: "Passive", // Active / Passive
+    installationDate: "10-08-2024",
+    status: "Occupied", // Free / Occupied
   },
 ];
 
